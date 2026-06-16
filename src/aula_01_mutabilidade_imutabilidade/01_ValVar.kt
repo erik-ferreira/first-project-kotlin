1fecfae146fb60aaaba197108c7636ec1ee92846
erik-ferreira <erikdesouzagf@gmail.com>
2026-06-15 23:33:58 -0300

    create first package variables

---
+++ src/aula_01_mutabilidade_imutabilidade/01_ValVar.kt
@@ -0,0 +1,15 @@
+package aula_01_mutabilidade_imutabilidade
+
+fun main() {
+    // val = value | immutable - Cannot be changed
+    val name = "Erik de Souza"
+
+    // var = variable | mutable - Can be changed
+    var age = 24
+
+    // 2026
+    // 24 anos
+
+    // 2027
+    age = 25
+}
